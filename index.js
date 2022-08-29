2c01889ef0cb9aa6b85ee76555b9854c46960dee
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2022-08-29 23:23:57 +0300

    complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(array, eventName) {
+    const newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        let message =  `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+
+const countDown = i => { while(i >= 0) { console.log(i); i--; }};
